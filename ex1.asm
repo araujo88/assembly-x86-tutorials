9556d8f177dca4a153d00d3213cf354452ed437a
Leonardo Araujo <leonardo.aa88@gmail.com>
2022-05-15 18:29:06 -0300

    Example 1 comments

--- ex1.asm
+++ ex1.asm
@@ -1,6 +1,6 @@
 global _start
 _start:
-    mov eax, 1
-    mov ebx, 42
-    sub ebx, 29
-    int 0x80
+    mov eax, 1  ; move the value 1 into the register eax
+    mov ebx, 42 ; move the value 42 into the register ebx
+    sub ebx, 29 ; subtract 29 from the register ebx
+    int 0x80    ; interrupt system call
